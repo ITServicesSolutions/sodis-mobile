ef45e9d6baaf83bcc3be6865c7c9b88236314d72
senado-09 <senadomonhedo@outlook.fr>
2025-10-03 10:11:43 +0100

    Final Update on Expo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,24 @@
 import { Stack, Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import { useTranslation } from "react-i18next";
 
 export default function NotFoundScreen() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn&apos;t exist.</Text>
+        <Text style={styles.title}>
+          {t("not_found.page_not_exist")}
+        </Text>
 
         {/* Link avec asChild pour éviter les warnings */}
         <Link href="/tabs" asChild>
-          <Text style={styles.linkText}>Go to home screen!</Text>
+          <Text style={styles.linkText}>
+            {t("not_found.go_home")}
+          </Text>
         </Link>
       </View>
     </>
